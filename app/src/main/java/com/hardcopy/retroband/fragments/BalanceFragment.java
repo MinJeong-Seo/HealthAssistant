33575f9546c842d3a5791f6c7740eea9e16863bf
MinJeong-Seo <slow7928@gmail.com>
2020-03-19 19:46:22 +0900

    add balance alarm sound button

--- app/src/main/java/com/hardcopy/retroband/fragments/BalanceFragment.java
+++ app/src/main/java/com/hardcopy/retroband/fragments/BalanceFragment.java
@@ -26,11 +26,15 @@ import com.hardcopy.retroband.R.layout;
 import com.hardcopy.retroband.R.string;
 import com.hardcopy.retroband.contents.ActivityReport;
 import com.hardcopy.retroband.contents.ContentManager;
+import com.hardcopy.retroband.contents.SoundManager;
 import com.hardcopy.retroband.logic.Analyzer;
 import com.hardcopy.retroband.utils.Logs;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
@@ -59,13 +63,20 @@ public class BalanceFragment extends Fragment implements IAdapterListener {
     private TextView mCalorieText = null;
     private TextView mWalksText = null;
     private TextView mBalanceText = null; //내가쓴부분
-    public static TextView mBalanceLevel_l2=null;//내가쓴부분
-    public static TextView mBalanceLevel_l1=null;//내가쓴부분
-    public static TextView mBalanceLevel_center=null;//내가쓴부분
-    public static TextView mBalanceLevel_R1=null;//내가쓴부분
-    public static TextView mBalanceLevel_R2=null;//내가쓴부분
+    public static TextView mBalanceLevel_l2 = null;//내가쓴부분
+    public static TextView mBalanceLevel_l1 = null;//내가쓴부분
+    public static TextView mBalanceLevel_center = null;//내가쓴부분
+    public static TextView mBalanceLevel_R1 = null;//내가쓴부분
+    public static TextView mBalanceLevel_R2 = null;//내가쓴부분
     //private ListView mTimelineList = null;
     private TimelineAdapter mTimelineListAdapter = null;
+
+    //AlarmSound 내가쓴부분
+    SoundPool soundPool;
+    SoundManager soundManager;
+    Button button;
+    boolean play;
+    int playSoundId;
 
 
     // Auto-refresh timer
@@ -82,6 +93,7 @@ public class BalanceFragment extends Fragment implements IAdapterListener {
     }
 
 
+
     /*****************************************************
      *	Overrided methods
      ******************************************************/
@@ -92,20 +104,20 @@ public class BalanceFragment extends Fragment implements IAdapterListener {
 
         View rootView = inflater.inflate(layout.fragment_balance, container, false);
 
-        mRenderStatistics = (RenderingStatistics)rootView.findViewById(R.id.render_Bstatistics);
+        mRenderStatistics = (RenderingStatistics) rootView.findViewById(R.id.render_Bstatistics);
 
 
         mCalorieText = (TextView) rootView.findViewById(R.id.text_Bcontent_calorie);
         mCalorieText.setText("0");
         mWalksText = (TextView) rootView.findViewById(R.id.text_content_works);
         mWalksText.setText("0");
-        mBalanceText=(TextView) rootView.findViewById(R.id.text_content_balance); //내가쓴부분
+        mBalanceText = (TextView) rootView.findViewById(R.id.text_content_balance); //내가쓴부분
         mBalanceText.setText("0"); //내가쓴부분
-        mBalanceLevel_l2=(TextView)rootView.findViewById(R.id.balance_l2);//내가쓴부분
-        mBalanceLevel_l1=(TextView)rootView.findViewById(R.id.balance_l1);//내가쓴부분
-        mBalanceLevel_center=(TextView)rootView.findViewById(R.id.balance_center);//내가쓴부분
-        mBalanceLevel_R1=(TextView)rootView.findViewById(R.id.balance_r1);//내가쓴부분
-        mBalanceLevel_R2=(TextView)rootView.findViewById(R.id.balance_r2);//내가쓴부분
+        mBalanceLevel_l2 = (TextView) rootView.findViewById(R.id.balance_l2);//내가쓴부분
+        mBalanceLevel_l1 = (TextView) rootView.findViewById(R.id.balance_l1);//내가쓴부분
+        mBalanceLevel_center = (TextView) rootView.findViewById(R.id.balance_center);//내가쓴부분
+        mBalanceLevel_R1 = (TextView) rootView.findViewById(R.id.balance_r1);//내가쓴부분
+        mBalanceLevel_R2 = (TextView) rootView.findViewById(R.id.balance_r2);//내가쓴부분
 
 
         // TODO: If you need to show activity data as list, use below code
@@ -117,12 +129,40 @@ public class BalanceFragment extends Fragment implements IAdapterListener {
 		mTimelineList.setAdapter(mTimelineListAdapter);
 		*/
 
+        //AlarmSound 내가쓴부분
+        button = rootView.findViewById(R.id.button_alarm);
+        //롤리팝 이상 버전일 경우
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            soundPool = new SoundPool.Builder().build();
+        } else {
+            //롤리팝 이하 버전일 경우
+            // new SoundPool(1번,2번,3번)
+            // 1번 - 음악 파일 갯수
+            // 2번 - 스트림 타입
+            // 3번 - 음질
+            soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        }
+        soundManager = new SoundManager(mContext, soundPool);
+        soundManager.addSound(0, R.raw.alarm_01);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!play) {
+                    playSoundId = soundManager.playSound(0);
+                    play = true;
+                } else {
+                    soundManager.pauseSound(0);
+                    play = false;
+                }
+            }
+        });
+
         return rootView;
     }
 
     @Override
     public void OnAdapterCallback(int msgType, int arg0, int arg1, String arg2, String arg3, Object arg4) {
-        switch(msgType) {
+        switch (msgType) {
             case IAdapterListener.CALLBACK_xxx:
                 // TODO:
                 //if(arg4 != null)
@@ -140,7 +180,7 @@ public class BalanceFragment extends Fragment implements IAdapterListener {
     @Override
     public void onStop() {
         super.onStop();
-        if(mRefreshTimer != null) {
+        if (mRefreshTimer != null) {
             mRefreshTimer.cancel();
             mRefreshTimer = null;
         }
@@ -152,10 +192,11 @@ public class BalanceFragment extends Fragment implements IAdapterListener {
      ******************************************************/
     /**
      * Initialize rendering view
-     * @return	boolean		is initialized or not
+     *
+     * @return boolean        is initialized or not
      */
     private boolean checkRenderView() {
-        if(mRenderStatistics != null) {
+        if (mRenderStatistics != null) {
             mRenderStatistics.initializeGraphics(0);
             return true;
         }
@@ -170,10 +211,11 @@ public class BalanceFragment extends Fragment implements IAdapterListener {
     /**
      * Show sum of calorie and sum of walk count
      * Service triggers this at every sync
+     *
      * @param object
      */
     public void showActivityReport(ActivityReport object) {
-        if(object != null) {
+        if (object != null) {
             String str = String.format("%,.0f", object.mSumOfCalorie);
             mCalorieText.setText(str);
             mWalksText.setText(Integer.toString(object.mShakeActionCount));
@@ -183,42 +225,42 @@ public class BalanceFragment extends Fragment implements IAdapterListener {
     }
 
     public void addMessage(ActivityReport object) {
-        if(object != null && mTimelineListAdapter != null) {
+        if (object != null && mTimelineListAdapter != null) {
             mTimelineListAdapter.addObject(object);
             mTimelineListAdapter.notifyDataSetChanged();
         }
     }
 
     public void addMessageOnTop(ActivityReport object) {
-        if(object != null && mTimelineListAdapter != null) {
+        if (object != null && mTimelineListAdapter != null) {
             mTimelineListAdapter.addObjectOnTop(object);
             mTimelineListAdapter.notifyDataSetChanged();
         }
     }
 
     public void addMessageAll(ArrayList<ActivityReport> objList) {
-        if(objList != null && mTimelineListAdapter != null) {
+        if (objList != null && mTimelineListAdapter != null) {
             mTimelineListAdapter.addObjectAll(objList);
             mTimelineListAdapter.notifyDataSetChanged();
         }
     }
 
     public void deleteMessage(int id) {
-        if(mTimelineListAdapter != null) {
+        if (mTimelineListAdapter != null) {
             mTimelineListAdapter.deleteObject(id);
             mTimelineListAdapter.notifyDataSetChanged();
         }
     }
 
     public void deleteMessageByType(int type) {
-        if(mTimelineListAdapter != null) {
+        if (mTimelineListAdapter != null) {
             mTimelineListAdapter.deleteObjectByType(type);
             mTimelineListAdapter.notifyDataSetChanged();
         }
     }
 
     public void deleteMessageAll() {
-        if(mTimelineListAdapter != null) {
+        if (mTimelineListAdapter != null) {
             mTimelineListAdapter.deleteObjectAll();
             mTimelineListAdapter.notifyDataSetChanged();
         }
@@ -232,17 +274,19 @@ public class BalanceFragment extends Fragment implements IAdapterListener {
      * Auto-refresh Timer
      */
     private class RefreshTimerTask extends TimerTask {
-        public RefreshTimerTask() {}
+        public RefreshTimerTask() {
+        }
 
         @Override
         public void run() {
-            if(mHandler != null) {
+            if (mHandler != null) {
                 mHandler.post(new Runnable() {
                     public void run() {
-                        }
+                    }
                 });
             }
         }
     }
+
 
 }
